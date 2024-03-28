d7c5f22fb42b9e9a44a4da26aa7d73bd099e8375
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-03-28 16:10:36 -0400

    Update client.py
    [v1.1.0] - [03/28/2024]
    
    ## Changed
    
    Switched from `httpx `to `requests`.
    
    ## Improved
    
    `client.py` now uses requests instead of `httpx`.
    
    ## Fixed
    
    Response processing in `client.py` now explicitly handles newline characters (\n) for better output formatting.

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
-import httpx
 import json
-from config import load_config
+import requests
 from loading import Loading
+from config import load_config
 
 print("------------------------------------------------------------------\n")
 print("                        Mistral AI Toolkit                        \n")     
 print("               API Wrapper & Command-line Interface               \n")   
-print("                       [v1.0.0] by @rmncldyo                      \n")  
+print("                       [v1.1.0] by @rmncldyo                      \n")  
 print("------------------------------------------------------------------\n")
 
 class Client:
@@ -15,6 +15,7 @@ class Client:
         self.api_key = api_key if api_key else self.config.get('api_key')
         self.version = self.config.get('version')
         self.base_url = self.config.get('base_url')
+        self.base_model = self.config.get('base_model')
         self.chat_endpoint = self.config.get('chat_endpoint')
         self.timeout = self.config.get('timeout')
         self.headers = {
@@ -23,49 +24,48 @@ class Client:
             'Accept': 'application/json'
         }
 
-    async def post(self, endpoint, data):
+    def post(self, endpoint, data):
         loading = Loading()
         url = f"{self.base_url}/{self.version}/{endpoint}"
         try:
             loading.start()
-            async with httpx.AsyncClient(timeout=self.timeout) as client:
-                response = await client.post(url, json=data, headers=self.headers)
-                response.raise_for_status()
-                return response.json()["choices"][0]["message"]["content"]
+            response = requests.post(url, json=data, headers=self.headers)
+            response.raise_for_status()
+            return response.json()["choices"][0]["message"]["content"].replace("\\n", "\n")
         except Exception as e:
             print(f"HTTP Error: {e}")
             raise
         finally:
-            await loading.stop()
+            loading.stop()
 
-    async def stream_post(self, endpoint, data):
+    def stream_post(self, endpoint, data):
         loading = Loading()
         url = f"{self.base_url}/{self.version}/{endpoint}"
         full_response = []
         try:
             loading.start()
-            async with httpx.AsyncClient(timeout=httpx.Timeout(timeout=None, connect=self.timeout)) as client:
-                async with client.stream("POST", url, json=data, headers=self.headers) as response:
-                    response.raise_for_status()
-                    await loading.stop()
-                    response_content = ""
-                    print("Assistant: ", end="", flush=True)
-                    async for line in response.aiter_lines():
-                        if line:
-                            try:
-                                line = line.strip()
-                                if 'data: ' in line:
-                                    json_data = line.split('data: ')[1]
-                                    data_dict = json.loads(json_data)
-                                    print(data_dict["choices"][0]["delta"]["content"], end="", flush=True)
-                                    response_content += data_dict["choices"][0]["delta"]["content"]
-                            except json.JSONDecodeError:
-                                pass
-                    full_response.append(response_content)
-                    print()
-                    return full_response[0]
+            response = requests.post(url, json=data, headers=self.headers, stream=True)
+            response.raise_for_status()
+            loading.stop()
+            response_content = ""
+            print("Assistant: ", end="", flush=True)
+            for line in response.iter_lines():
+                if line:
+                    try:
+                        line = str(line.strip())
+                        if 'data: ' in line:
+                            json_data = line.split('data: ')[1][:-1]
+                            data_dict = json.loads(json_data)
+                            print(data_dict["choices"][0]["delta"]["content"].replace("\\n", "\n"), end="", flush=True)
+                            response_content += data_dict["choices"][0]["delta"]["content"].replace("\\n", "\n")
+                    except json.JSONDecodeError:
+                        pass
+            full_response.append(response_content)
+            print()
+            return full_response[0]
         except Exception as e:
+            loading.stop()
             print(f"Stream HTTP Error: {e}")
             raise
         finally:
-            await loading.stop()
+            loading.stop()
