7820e5f9f81a770adcb16ff6409aad78eca3dd81
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-03-28 16:08:22 -0400

    Update mistral.py
    [v1.1.0] - [03/28/2024]
    
    ## Changed
    
    `mistral.py` was split into two classes: Chat and Text, separating the functionalities for more structured code and potentially different use cases.
    
    ## Improved
    
    `client.py` now uses requests instead of `httpx`.
    
    ## Fixed
    
    Response processing in `client.py` now explicitly handles newline characters (\n) for better output formatting.
    
    ## Added
    
    Support for a default model through the `base_model` configuration, allowing users to omit specifying a model for each request.

--- mistral.py
+++ mistral.py
@@ -1,55 +1,105 @@
-import asyncio
 from client import Client
 
-class Mistral:
+class Chat:
     def __init__(self):
         self.client = None
 
-    def chat(self, api_key=None, model="mistral-large-latest", json=None, system_prompt=None, temperature=None, top_p=None, max_tokens=None, stream=None, safe_prompt=None, random_seed=None):
+    def run(self, api_key=None, model=None, prompt=None, stream=None, json=None, system_prompt=None, max_tokens=None, temperature=None, top_p=None, random_seed=None, safe_prompt=None):
         
         self.client = Client(api_key=api_key)
+        self.model = model if model else self.client.config.get('base_model')
 
-        async def conversation():
-            conversation_history = []
-            if system_prompt:
-                conversation_history.append({"role": "system", "content": system_prompt})
+        conversation_history = []
 
-            print("Assistant: Hello! How can I assist you today?")
-            while True:
+        if system_prompt:
+            conversation_history.append({"role": "system", "content": system_prompt})
+
+        print("Assistant: Hello! How can I assist you today?")
+        while True:
+            if prompt:
+                user_input = prompt.strip()
+                print(f"User: {user_input}")
+                prompt = None
+            else:
                 user_input = input("User: ").strip()
                 if user_input.lower() in ['exit', 'quit']:
                     print("\nThank you for using the Mistral AI toolkit. Have a great day!")
                     break
-                
-                if json:
-                    conversation_history.append({"role": "user", "response_format": {"type": "json_object"}, "content": user_input + ". Only respond in JSON."})
-                else:
-                    conversation_history.append({"role": "user", "content": user_input})
 
-                data = {
-                    "model": model,
-                    "messages": conversation_history,
-                    "temperature": temperature,
-                    "top_p": top_p,
-                    "max_tokens": max_tokens,
-                    "stream": stream,
-                    "safe_prompt": safe_prompt,
-                    "random_seed": random_seed
-                }
-                data = {k: v for k, v in data.items() if v is not None}
-                endpoint = self.client.config.get('chat_endpoint')
-
-                if stream:
-                    response = await self.client.stream_post(endpoint, data)
-                else:
-                    response = await self.client.post(endpoint, data)
-                    print(f"Assistant: {response}")
-                conversation_history.append({"role": "assistant", "content": response})
-
-        try:
-            asyncio.run(conversation())
-        except RuntimeError as e:
-            if str(e) == 'This conversation loop is already running':
-                print("Event conversation loop is already running. Skipping asyncio.run() to avoid conflict.")
+                if not user_input:
+                    print("Invalid input detected. Please enter a valid message.")
+                    continue
+            
+            if json:
+                conversation_history.append({"role": "user", "response_format": {"type": "json_object"}, "content": user_input + ". Only respond in JSON."})
             else:
-                raise
+                conversation_history.append({"role": "user", "content": user_input})
+
+            data = {
+                "model": self.model,
+                "messages": conversation_history,
+                "temperature": temperature,
+                "top_p": top_p,
+                "max_tokens": max_tokens,
+                "stream": stream,
+                "safe_prompt": safe_prompt,
+                "random_seed": random_seed
+            }
+            data = {k: v for k, v in data.items() if v is not None}
+            endpoint = self.client.config.get('chat_endpoint')
+
+            if stream:
+                response = self.client.stream_post(endpoint, data)
+            else:
+                response = self.client.post(endpoint, data)
+                print(f"Assistant: {response}")
+            conversation_history.append({"role": "assistant", "content": response})
+
+class Text:
+    def __init__(self):
+        self.client = None
+
+    def run(self, api_key=None, model=None, prompt=None, stream=None, json=None, system_prompt=None, max_tokens=None, temperature=None, top_p=None, random_seed=None, safe_prompt=None):
+        
+        self.client = Client(api_key=api_key)
+        self.model = model if model else self.client.config.get('base_model')
+
+        if not prompt:
+            print("Error: { Invalid input detected }. Please enter a valid message.")
+            exit(1)
+
+        conversation_history = []
+        
+        if system_prompt:
+            conversation_history.append({"role": "system", "content": system_prompt})
+
+        user_input = prompt.strip()
+        
+        print("Assistant: Hello! How can I assist you today?")
+        print(f"User: {user_input}")
+        
+        if json:
+            conversation_history.append({"role": "user", "response_format": {"type": "json_object"}, "content": user_input + ". Only respond in JSON."})
+        else:
+            conversation_history.append({"role": "user", "content": user_input})
+
+        data = {
+            "model": self.model,
+            "messages": conversation_history,
+            "temperature": temperature,
+            "top_p": top_p,
+            "max_tokens": max_tokens,
+            "stream": stream,
+            "safe_prompt": safe_prompt,
+            "random_seed": random_seed
+        }
+        data = {k: v for k, v in data.items() if v is not None}
+        endpoint = self.client.config.get('chat_endpoint')
+
+        if stream:
+            response = self.client.stream_post(endpoint, data)
+        else:
+            response = self.client.post(endpoint, data)
+            print(f"Assistant: {response}")
+        
+        print("\nThank you for using the Mistral AI toolkit. Have a great day!")
