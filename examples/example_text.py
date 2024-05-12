1cff5629e7f95f1f3fe42f2e02536abaf0be47a9
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-05-11 22:52:46 -0400

    Update example_text.py

--- examples/example_text.py
+++ examples/example_text.py
@@ -1,3 +1,3 @@
 from mistral import Text
 
-Text().run(prompt="Which one is heavier a pound of iron or a kilogram of feathers?")
+Text().run(prompt="Which one is heavier, a pound of iron or a kilogram of feathers?")
